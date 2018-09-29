6219e9c2b762caccca6569f3536307d950230b71
toe_head2001 <toe_head2001@yahoo.com>
2018-09-29 17:07:17 -0600

    Update assembly info

--- BulletinBoardTrim/Properties/AssemblyInfo.cs
+++ BulletinBoardTrim/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("bulletin board trim border")]
 [assembly: AssemblyCompany("toe_head2001")]
 [assembly: AssemblyProduct("Bulletin Board Trim")]
-[assembly: AssemblyCopyright("Copyright © toe_head2001")]
+[assembly: AssemblyCopyright("Copyright © 2018 toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
